01901d3fe350abe77a2b58fd60924ca7eb391013
ivanburda1986 <burda.ivan@gmail.com>
2019-09-27 00:13:37 +0200

    javascript challenges from th fcc

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,18 @@
-function randomRange(myMin, myMax) {
-
-    return(Math.floor(Math.random()*(myMax - myMin + 1))+myMin); // Change this line
-  
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+    newArray.push([]);
+    // Pushes the current row, which now has n zeroes in it, to the array
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      newArray[i].push(0);
+    }
   }
+  return newArray;
+}
 
-  console.log(randomRange(5, 15)); 
+let matrix = zeroArray(3, 2);
+console.log(matrix);
