7ddbd8ad1082157b844d80c7dd2b0670b6aad46e
iBurda <ivan.burda@erstegroup.com>
2019-10-04 17:17:02 +0200

    working on the falsy bouncer

---
+++ javascript/falsyBouncer.js
@@ -0,0 +1,17 @@
+function bouncer(arr) {
+    let values = arr;
+    const falsies = [false, null, 0, "", undefined, NaN];
+    for (let i = 0; i < arr.length; i += 1){
+        for (let j = 0; j < falsies.length; j += 1){
+            if (values[i] === falsies[j]){
+                values.splice(i, 1);
+            }
+        }
+    }
+    console.log (values);
+  }
+  
+  bouncer([7, "ate", "", false, 9]); //should return [7, "ate", 9].
+  bouncer(["a", "b", "c"]); // should return ["a", "b", "c"].
+  bouncer([false, null, 0, NaN, undefined, ""]);// should return [].
+  bouncer([1, null, NaN, 2, undefined]); // should return [1, 2].
