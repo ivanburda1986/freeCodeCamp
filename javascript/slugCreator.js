06fb07b11129481ca132ccb21449ea5d354dcbef
ivanburda1986 <burda.ivan@gmail.com>
2019-10-30 20:41:17 +0100

    implement the slug creator

--- javascript/slugCreator.js
+++ javascript/slugCreator.js
@@ -1,13 +1,21 @@
 // the global variable
-var globalTitle = "Winter Is Coming";
+var globalTitle = " Winter Is  Coming";
 
 // Add your code below this line
+const isWord = (callback) => {
+    if (callback.length >0){
+        return true;
+    } return false;
+}
+
 function urlSlug(title) {
-const words = title.split(" ");
-words.array.forEach(element => {
-    element.toLowerCase();
-});
-console.log(words);
+let words = title.split(' ');
+let newWords = [];
+  words.forEach((element) => {
+     newWords.push(element.trim().toLowerCase());
+  });
+newWords = newWords.filter(isWord);
+return(newWords.join('-'));
 }
 // Add your code above this line
 
