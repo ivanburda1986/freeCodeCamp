191bbfb846f9f13613c696cdbe0a79a155934b88
ivanburda1986 <burda.ivan@gmail.com>
2019-11-01 00:50:23 +0100

    challenge3

---
+++ javascript/challenges/02-symmetric-difference-sol2.js
@@ -0,0 +1,8 @@
+function diffArray(arr1, arr2) {
+    console.log(arr1.concat(arr2).filter(item => !arr1.includes(item) || !arr2.includes(item)));
+  }
+
+
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]); //  should return [4].
+diffArray([1, 'calf', 3, 'piglet'], [7, 'filly']); // should return [1, "calf", 3, "piglet", 7, "filly"].
---
+++ javascript/challenges/02-symmetric-difference.js
@@ -0,0 +1,25 @@
+const compare = (value, array) => {
+  if (array.indexOf(value) > -1) {
+    return true;
+  }
+  return false;
+};
+
+function diffArray(arr1, arr2) {
+  const newArr = [];
+  for (let i = 0; i < arr1.length; i += 1) {
+    if (!compare(arr1[i], arr2)) {
+      newArr.push(arr1[i]);
+    }
+  }
+  for (let i = 0; i < arr2.length; i += 1) {
+    if (!compare(arr2[i], arr1)) {
+      newArr.push(arr2[i]);
+    }
+  }
+  return (newArr);
+}
+
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]); // should return [4].
+diffArray([1, 'calf', 3, 'piglet'], [7, 'filly']); // should return [1, "calf", 3, "piglet", 7, "filly"].
