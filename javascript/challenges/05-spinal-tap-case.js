7ddd5eb084b4b4d1fa52ed51069e5ee55b709841
iBurda <ivan.burda@erstegroup.com>
2019-11-23 19:43:04 +0100

    working on the challenge

--- javascript/challenges/05-spinal-tap-case.js
+++ javascript/challenges/05-spinal-tap-case.js
@@ -1,30 +1,13 @@
-const isWord = (callback) => {
-  if (callback.length > 0) {
-    return true;
-  } return false;
-};
-
-
-
-const normalize = (string) => {
-  const stringChars = string.split('');
-    stringChars.forEach(element => {
-        if(element === element.toUpperCase()){
-           stringChars.splice(stringChars.indexOf(element), 0, "");
-        }
-    })
-    console.log(stringChars);
-};
-
 function spinalCase(str) {
-  const words = str.split(' ');
-  let newWords = [];
-  words.forEach((element) => {
-    newWords.push(element.trim().toLowerCase());
-  });
-  newWords = newWords.filter(isWord);
-  console.log(newWords.join('-'));
+ const regex = /\s+/g;
+ for (const element in str){
+   if (element === element.toUpperCase){
+     str.
+   }
+ }
+ console.log(spaceCount.length);
+
 }
 
 spinalCase('This Is Spinal Tap');
-spinalCase('thisIsSpinalTap');
+
