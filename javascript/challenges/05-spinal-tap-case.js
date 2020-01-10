1366a222fc77de55859d823e59d3f7168a61c022
iBurda <ivan.burda@erstegroup.com>
2020-01-10 15:43:40 +0100

    progressing with the spinal tap

--- javascript/challenges/05-spinal-tap-case.js
+++ javascript/challenges/05-spinal-tap-case.js
@@ -1,13 +1,32 @@
 function spinalCase(str) {
- const regex = /\s+/g;
- for (const element in str){
-   if (element === element.toUpperCase){
-     str.
-   }
- }
- console.log(spaceCount.length);
+// const regex = /\s+/g;
+let chars = str.split("");
+chars[0] = chars[0].toLowerCase();
 
+for(element in chars) {
+  if (chars[element] === " " || chars[element] === "_"){
+    chars[element] = "-";
+    }
+  };
+
+for (element in chars){
+  let regex = /[a-z]/g;
+  if (chars[element] === chars[element].toUpperCase()){
+      if(regex.test(chars[element-1])){
+        chars.splice(element-1, 0, "-");
+      }
+    }
+  }
+  return chars;
 }
 
-spinalCase('This Is Spinal Tap');
+console.log(spinalCase('This Is Spinal Tap'));
+console.log(spinalCase('thisIsSpinalTap'));
+console.log(spinalCase('The_Andy_Griffith_Show'));
+console.log(spinalCase('Teletubbies say Eh-oh'));
+console.log(spinalCase('AllThe-small Things'));
 
+/*
+;
+
+*/
