c710ec92968d2ff7d8d1750b3c2b56ad80b2acc8
iBurda <ivan.burda@erstegroup.com>
2020-01-10 17:13:53 +0100

    progress

--- javascript/challenges/05-spinal-tap-case.js
+++ javascript/challenges/05-spinal-tap-case.js
@@ -3,21 +3,19 @@ function spinalCase(str) {
 let chars = str.split("");
 chars[0] = chars[0].toLowerCase();
 
-for(element in chars) {
-  if (chars[element] === " " || chars[element] === "_"){
-    chars[element] = "-";
-    }
-  };
+  for(element in chars) {
+    if (chars[element] === " " || chars[element] === "_"){
+      chars[element] = "-";
+      }
+    };
 
-for (element in chars){
-  let regex = /[a-z]/g;
-  if (chars[element] === chars[element].toUpperCase()){
-      if(regex.test(chars[element-1])){
-        chars.splice(element-1, 0, "-");
+  for (element in chars){
+    if (chars[element] === chars[element].toUpperCase()){
+      console.log('1');
       }
     }
-  }
-  return chars;
+
+return chars;
 }
 
 console.log(spinalCase('This Is Spinal Tap'));
@@ -27,6 +25,13 @@ console.log(spinalCase('Teletubbies say Eh-oh'));
 console.log(spinalCase('AllThe-small Things'));
 
 /*
-;
-
+for (element in chars){
+  let regex = /[a-z]/g;
+  if (chars[element] === chars[element].toUpperCase()){
+      if(regex.test(chars[element-1])){
+        chars.splice(element-1, 0, "?");
+      }
+    }
+    return chars;
+  }
 */
