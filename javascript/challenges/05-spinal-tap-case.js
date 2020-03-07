16a0c3f475dde5645a5cc3ad5ee6c4eb988bec66
ivanburda1986 <burda.ivan@gmail.com>
2020-03-07 21:26:46 +0100

    added the remaining tests

--- javascript/challenges/05-spinal-tap-case.js
+++ javascript/challenges/05-spinal-tap-case.js
@@ -1,30 +1,36 @@
-const isWord = (callback) => {
+/*const isWord = (callback) => {
   if (callback.length > 0) {
     return true;
-  } return false;
+  }
+  return false;
 };
 
+newWords = newWords.filter(isWord);*/
 
 
 const normalize = (string) => {
   const stringChars = string.split('');
-    stringChars.forEach(element => {
-        if(element === element.toUpperCase()){
-           stringChars.splice(stringChars.indexOf(element), 0, "");
-        }
-    })
-    console.log(stringChars);
+  stringChars.forEach((element) => {
+    if (element === element.toUpperCase()) {
+      stringChars.splice(stringChars.indexOf(element), 0, '');
+    }
+  });
+  console.log(stringChars);
 };
 
 function spinalCase(str) {
   const words = str.split(' ');
   let newWords = [];
   words.forEach((element) => {
-    newWords.push(element.trim().toLowerCase());
+    newWords.push(element.trim());
   });
-  newWords = newWords.filter(isWord);
+
+  console.log(newWords);
   console.log(newWords.join('-'));
 }
 
 spinalCase('This Is Spinal Tap');
 spinalCase('thisIsSpinalTap');
+spinalCase('The_Andy_Griffith_Show');
+spinalCase('Teletubbies say Eh-oh');
+spinalCase('AllThe-small Things');
