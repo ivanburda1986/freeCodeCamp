4477eead4da718448f01519c6e575773abf5c450
ivanburda1986 <burda.ivan@gmail.com>
2020-09-26 22:18:44 +0200

    turned up int uppercase to make it clearer

--- javascript/challenges/07-search-and-replace.js
+++ javascript/challenges/07-search-and-replace.js
@@ -2,10 +2,9 @@ function myReplace(str, before, after) {
   const regexCap = new RegExp(`[${before.charAt(0).toUpperCase()}]${before.substring(1)}`);
 
   function modify(what, how) {
-    return how === 'up' ? what.charAt(0).toUpperCase() + what.slice(1) : what.charAt(0).toLowerCase() + what.slice(1);
+    return how === 'toUpperCase' ? what.charAt(0).toUpperCase() + what.slice(1) : what.charAt(0).toLowerCase() + what.slice(1);
   }
-  return regexCap.test(str) ? str.replace(modify(before, 'up'), modify(after, 'up')) : str.replace(modify(before), modify(after));
-
+  return regexCap.test(str) ? str.replace(modify(before, 'toUpperCase'), modify(after, 'toUpperCase')) : str.replace(modify(before), modify(after));
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "Leaped");
