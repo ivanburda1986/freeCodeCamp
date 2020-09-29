770758c7c20ffa3ce1a769458772499dc7df1366
ivanburda1986 <burda.ivan@gmail.com>
2020-09-29 23:57:27 +0200

    solution ends in stack overflow

--- javascript/challenges/14-common-multiplier.js
+++ javascript/challenges/14-common-multiplier.js
@@ -1,22 +1,22 @@
+//This solution unfortunately ends up in maximum stact overflow :-(
+
 let arg1Multiples = [];
 let arg2Multiples = [];
 let commonMultiples = [];
 let betweeners = [];
-
-
+let finished = 0;
 
 function sortAndSetInitial(arr) {
   let numbers = arr.sort((a, b) => a - b);
   arg1Multiples.push(numbers[0]);
   arg2Multiples.push(numbers[1]);
-};
+}
 
 function getBetweeners() {
   for (let i = arg1Multiples[0] + 1; i < arg2Multiples[0]; i++) {
     betweeners.push(i);
   }
-};
-
+}
 
 function optimiseArrays() {
   if (arg1Multiples.length > 2) {
@@ -33,46 +33,44 @@ function optimiseArrays() {
   }
 }
 
-
 function arg1NextMultiple() {
   arg1Multiples.push(arg1Multiples[arg1Multiples.length - 1] + arg1Multiples[0]);
-};
+}
 
 function arg2NextMultiple() {
   arg2Multiples.push(arg2Multiples[arg2Multiples.length - 1] + arg2Multiples[0]);
-};
+}
 
 function isDivisibleByBetweeners() {
   let isDivisible = true;
-  betweeners.forEach(number => {
+  betweeners.forEach((number) => {
     if (commonMultiples[commonMultiples.length - 1] % number !== 0) {
       isDivisible = false;
       arg1NextMultiple();
       arg2NextMultiple();
       getNextCommonMultiple();
     }
-  })
+  });
   if (isDivisible) {
+    finished = 1;
     console.log(commonMultiples[commonMultiples.length - 1]);
-    return (commonMultiples[commonMultiples.length - 1]);
+    return commonMultiples[commonMultiples.length - 1];
   }
 }
 
-
 function getNextCommonMultiple() {
-  optimiseArrays()
+  optimiseArrays();
   if (arg1Multiples[arg1Multiples.length - 1] < arg2Multiples[arg2Multiples.length - 1]) {
     arg1NextMultiple();
-    getNextCommonMultiple();
+    // getNextCommonMultiple();
   } else if (arg1Multiples[arg1Multiples.length - 1] > arg2Multiples[arg2Multiples.length - 1]) {
     arg2NextMultiple();
-    getNextCommonMultiple();
+    // getNextCommonMultiple();
   } else if (arg1Multiples[arg1Multiples.length - 1] === arg2Multiples[arg2Multiples.length - 1]) {
     commonMultiples.push(arg1Multiples[arg1Multiples.length - 1]);
     isDivisibleByBetweeners();
   }
 }
-
 
 function smallestCommons(arr) {
   sortAndSetInitial(arr);
@@ -80,5 +78,4 @@ function smallestCommons(arr) {
   getNextCommonMultiple();
 }
 
-
-smallestCommons([1, 11]);
+smallestCommons([1, 10]);
