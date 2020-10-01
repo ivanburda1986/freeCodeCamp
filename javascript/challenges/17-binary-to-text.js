3978e0c053ad46e46198002ac9e97eabdab6bf42
ivanburda1986 <burda.ivan@gmail.com>
2020-10-01 23:06:54 +0200

    finished the convert binaries challenge

--- javascript/challenges/17-binary-to-text.js
+++ javascript/challenges/17-binary-to-text.js
@@ -1,6 +1,19 @@
 function binaryAgent(str) {
-  parseInt("01001001", 2)
-  String.fromCharCode("73")
+  let binaries = str.split(' ');
+  let decimals = [];
+  let letters = [];
+  binaries.forEach(binary => {
+    decimals.push(parseInt(binary, 2));
+  });
+  decimals.forEach(decimal => {
+    letters.push(String.fromCharCode(decimal));
+  })
+  return letters.join('');
 }
 
-binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+
+
+//Slice, splice, split
+//https: //www.freecodecamp.org/news/lets-clear-up-the-confusion-around-the-slice-splice-split-methods-in-javascript-8ba3266c29ae/
