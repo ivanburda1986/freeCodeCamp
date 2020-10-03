1cb924f748d8fe1dfc85b448aa613f7612b0a641
Ivan Burda <burda.ivan@gmail.com>
2020-10-04 00:32:41 +0200

    finished the intermediate algorith challenges

--- javascript/challenges/21-orbital-period.js
+++ javascript/challenges/21-orbital-period.js
@@ -18,3 +18,29 @@ function orbitalPeriod(bodies) {
 }
 
 console.log(orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]));
+
+//My simple but less transparent solution
+/* function orbitalPeriod(bodies) {
+  let result = [];
+  bodies.forEach((body) => {
+    result.push({
+      name: body.name,
+      orbitalPeriod: Math.round(
+        2 * Math.PI * Math.sqrt(Math.pow(6367.4447 + body.avgAlt, 3) / 398600.4418)
+      )
+    });
+  });
+  return result;
+} */
+
+//A nice solution from the forum
+function orbitalPeriod(a) {
+  return a.map((p) => {
+    return {
+      name: p.name,
+      orbitalPeriod: Math.round(
+        2 * Math.PI * Math.sqrt(Math.pow(p.avgAlt + 6367.4447, 3) / 398600.4418)
+      ),
+    };
+  });
+}
