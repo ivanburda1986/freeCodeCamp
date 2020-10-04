f79089d51adb7a5485ec544d6706048c3947fa32
ivanburda1986 <burda.ivan@gmail.com>
2020-10-04 23:41:43 +0200

    finished the roman numerals converter project

--- javascript/challenges/algorith-projects/02-roman-numerals.js
+++ javascript/challenges/algorith-projects/02-roman-numerals.js
@@ -1,67 +1,45 @@
 function convertToRoman(num) {
+
+  //How an arabic numeral gets expressed as a roman numeral depending on its position from the ends of the arabic number
   let romans = {
+    //Example: 1: ["I", "X", "C", "M"] -> How "1" gets expressed at 1st, 2nd, 3rd and 4th position from the end of the number to be converted
+    0: ["", "", "", ""],
     1: ["I", "X", "C", "M"],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-    6: [],
-    7: [],
-    8: [],
-    9: [],
+    2: ["II", "XX", "CC", "MM"],
+    3: ["III", "XXX", "CCC", "MMM"],
+    4: ["IV", "XL", "CD", "IV"],
+    5: ["V", "L", "D", "V"],
+    6: ["VI", "LX", "DC", "VM"],
+    7: ["VII", "LXX", "DCC", "VMM"],
+    8: ["VIII", "LXXX", "DCCC", "VMMM"],
+    9: ["IX", "XC", "CM", "IX"],
   };
 
-  let digits = [...num.toString()];
-  let length = digits.length;
+  //Amount of arabic numeral and the length of the number to be converted
+  let numerals = [...num.toString()];
+  let length = numerals.length;
 
-  //Get an object of positions from the end of the inputed number and digits at the positions
-  let positionsFromEndAndTheirDigits = {};
+  //Get an object of positions from the end of the inputed number and numerals at those positions
+  let positionsFromEndAndTheirNumerals = {};
   for (let i = length; i > 0; i--) {
-    positionsFromEndAndTheirDigits[i] = digits[length - i];
+    positionsFromEndAndTheirNumerals[i] = numerals[length - i];
   }
 
-  //Get an object with all individual digits in the inputed number and for each of them a position from the end
-  let digitsAndTheirPositionsFromEnd = [];
-  for (let i = 0; i < Object.keys(positionsFromEndAndTheirDigits).length; i++) {
-    digitsAndTheirPositionsFromEnd.push([
-      parseInt(Object.values(positionsFromEndAndTheirDigits)[i]),
-      parseInt(Object.keys(positionsFromEndAndTheirDigits)[i]),
+  //Get an object with all individual arabic numerals in the inputed number and for each of them a position from the end
+  let numeralsAndTheirPositionsFromEnd = [];
+  for (let i = 0; i < Object.keys(positionsFromEndAndTheirNumerals).length; i++) {
+    numeralsAndTheirPositionsFromEnd.push([
+      parseInt(Object.values(positionsFromEndAndTheirNumerals)[i]),
+      parseInt(Object.keys(positionsFromEndAndTheirNumerals)[i] - 1),
     ]);
   }
-  console.log(digitsAndTheirPositionsFromEnd);
-  //Replace each digit depending on its position from the end of the inputed number with the correct roman form
+
+  //Replace each arabic numeral depending on its position from the end of the inputed number with the correct roman numeral
   let fullRomanNumeral = [];
-  digitsAndTheirPositionsFromEnd.forEach((digit) => {
-    fullRomanNumeral.unshift(romans[digit[0]][digit[1]]);
+  numeralsAndTheirPositionsFromEnd.forEach((numeral) => {
+    fullRomanNumeral.unshift(romans[numeral[0]][numeral[1]]);
   });
-  console.log(fullRomanNumeral);
+  return (fullRomanNumeral.join(''));
 }
 
-convertToRoman(111);
-
-/* convertToRoman(2) should return "II".
-convertToRoman(3) should return "III".
-convertToRoman(4) should return "IV".
-convertToRoman(5) should return "V".
-convertToRoman(9) should return "IX".
-convertToRoman(12) should return "XII".
-convertToRoman(16) should return "XVI".
-convertToRoman(29) should return "XXIX".
-convertToRoman(44) should return "XLIV".
-convertToRoman(45) should return "XLV"
-convertToRoman(68) should return "LXVIII"
-convertToRoman(83) should return "LXXXIII"
-convertToRoman(97) should return "XCVII"
-convertToRoman(99) should return "XCIX"
-convertToRoman(400) should return "CD"
-convertToRoman(500) should return "D"
-convertToRoman(501) should return "DI"
-convertToRoman(649) should return "DCXLIX"
-convertToRoman(798) should return "DCCXCVIII"
-convertToRoman(891) should return "DCCCXCI"
-convertToRoman(1000) should return "M"
-convertToRoman(1004) should return "MIV"
-convertToRoman(1006) should return "MVI"
-convertToRoman(1023) should return "MXXIII"
-convertToRoman(2014) should return "MMXIV"
-convertToRoman(3999) should return "MMMCMXCIX" */
+console.log(convertToRoman(1492));
