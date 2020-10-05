f6360cdc98b31213237f751921e70fe3a454649c
ivanburda1986 <burda.ivan@gmail.com>
2020-10-06 00:11:44 +0200

    finished the phone validator

--- javascript/challenges/algorith-projects/04-phone-number-validator.js
+++ javascript/challenges/algorith-projects/04-phone-number-validator.js
@@ -1,5 +1,8 @@
 function telephoneCheck(str) {
-  let regex = /^(1\s)?$/;
+  let regex = /^(1\s|1)?([0-9]{3}|\([0-9]{3}\))(-|\s)?[0-9]{3}(-|\s)?[0-9]{4}$/;
+  return (regex.test(str));
 }
 
-telephoneCheck("555-555-5555");
+console.log(telephoneCheck("555-555-5555"));
+
+//A great regex learning resource: https://regexone.com/
